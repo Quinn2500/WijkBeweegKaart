7012afe9185699d57d760c854112fe58c905e62f
Unknown <q.vanveen@student.fontys.nl>
2020-03-09 15:54:23 +0100

    Created All Question Models

---
+++ WBK/DataModels/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModels
+{
+    class Location
+    {
+        public Location(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
